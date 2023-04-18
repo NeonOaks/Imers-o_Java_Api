a71f6f3da63c43c886df46ad13cb5ca0ac8bd130
@david.carvalho <david.carvalho@valecard.com.br>
2023-04-18 20:49:27 -0300

    As demais Linguagem Prog.

--- src/main/java/br/com/alura/linguagensapi/LinguagemController.java
+++ src/main/java/br/com/alura/linguagensapi/LinguagemController.java
@@ -21,7 +21,47 @@ public class LinguagemController {
             new Linguagem(
                 "PHP", 
                 "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 
-                2)
+                2),
+
+            new Linguagem(
+                "Python", 
+                "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_512x512.png", 
+                3),
+
+            new Linguagem(
+                "C#", 
+                "https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png", 
+                4),
+
+            new Linguagem(
+                "C++", 
+                "https://superconhecimento.com.br/wp-content/uploads/2021/11/Novo-C-1536x864.png", 
+                5),
+
+            new Linguagem(
+                "JavaScrit", 
+                "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_512x512.png", 
+                6),
+            
+            new Linguagem(
+                "Swift", 
+                "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/swift/swift_512x512.png", 
+                7),
+
+            new Linguagem(
+                "Go", 
+                "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go/go_512x512.png", 
+                8),
+
+            new Linguagem(
+                "SQL", 
+                "https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png", 
+                9),
+
+            new Linguagem(
+                "Ruby", 
+                "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/ruby/ruby_512x512.png", 
+                10)
            
         );
          
